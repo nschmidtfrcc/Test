8159317dc0e963f00568b411ee693ed150d9ebbe
nschmidtfrcc <134324541+nschmidtfrcc@users.noreply.github.com>
2023-06-04 19:07:01 -0600

    Add files via upload
    Starting file.

---
+++ Chap1d.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+   string firstName;
+   string lastName;
+   
+   cout << "Enter your first name: ";
+   cin >> firstName;
+   
+   cout << "Enter your last name: ";
+   cin >> lastName;
+   
+   cout << "Your name is " << firstName << " " << lastName << "." << endl;
+   
+} // end main
